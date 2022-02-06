c1c0ee254517f5c9f162014e01aaf42aedd6f27e
Oussema <Oussema.dhraief.17@gmail.com>
2022-02-06 18:21:09 +0000

    commit

--- Models/Product.js
+++ Models/Product.js
@@ -4,40 +4,40 @@ const ProductSchema = new mongoose.Schema({
         type: String,
         required: true,
         trim: true,
-        minLength :[5 ,'La référence doit être au moins 5 caractères']
+        minlength: [5, 'La référence doit être au moins 5 caractères']
     },
-    name :{
-        type : String,
-        required :[True , 'Insérez un nom'],
-        unique :true,
+    name: {
+        type: String,
+        required: [true, 'Insérez un nom'],
+        unique: true,
         trim: true,
-        minLength :[2, 'Le nom doit être au moins 2 caractères']
+        minlength: [2, 'Le nom doit être au moins 2 caractères']
     },
-    sizes :{
-        type : [Number],
-        required:True,
+    sizes: {
+        type: [Number],
+        required: true,
         trim: true,
     },
     description: {
         type: String,
-        required : false ,
+        required: false,
     },
-    category : {
-        type : String,
-        required : [True , 'Insérez la catégorie'],
-        unique :false,
-        trim :true,
-        minLength :[4 , 'La catégorie doit être au moins 4 caractères']
+    category: {
+        type: String,
+        required: [true, 'Insérez la catégorie'],
+        unique: false,
+        trim: true,
+        minlength: [4, 'La catégorie doit être au moins 4 caractères']
 
     },
-    subCategory : {
-        type : String,
-        required:[True , 'Insérez la sous-catégorie'],
-        unique :false,
-        trim :true,
-        minLength :[4 , 'La sous-catégorie doit être au moins 4 caractères']
-        }
+    subCategory: {
+        type: String,
+        required: [true, 'Insérez la sous-catégorie'],
+        unique: false,
+        trim: true,
+        minlength: [4, 'La sous-catégorie doit être au moins 4 caractères']
+    }
 
 })
 
-module.exports = mongoose.models.Note || mongoose.model('Product',ProductSchema)
+module.exports = mongoose.models.Product || mongoose.model('Product', ProductSchema)
--- utils/dbConnect.js
+++ utils/dbConnect.js
@@ -1,15 +1,21 @@
-import mongoose from "mongoose";
+const mongoose = require('mongoose');
 
 const connection = {};
+
 async function dbConnect() {
+
     if (connection.isConnected) {
         return;
     }
+    
     const db = await mongoose.connect(process.env.MONGO_URI , {
         useNewUrlParser:true,
         useUnifiedTopology:true,
     });
+
     connection.isConnected = db.connections[0].readyState;
-    console.log(connection.isConnected); 
+    console.log(connection.isConnected);
+
 }
+
 export default dbConnect;
