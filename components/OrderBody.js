1520744f7e1d5b726ed8e24b6d837def55c4e2a0
Oussema <Oussema.dhraief.17@gmail.com>
2022-02-06 14:58:06 +0000

    commit

--- components/OrderBody.js
+++ components/OrderBody.js
@@ -5,11 +5,11 @@ export default function OrderBody(){
         <tr className="border-b-[1px] border-zinc-300 h-fit">
             <td className="text-center p-3">Oussema Dhraief</td>
             <td className="text-center p-3">10 Janv. 2022</td>
-            <td colSpan={2} className="text-center p-3">Hamlock&#40;1&#41;, Flatline&#40;1&#41;, R-301&#40;1&#41;, Mastiff&#40;1&#41;, Deez&#40;1&#41;, Halouani&#40;1&#41;</td>
+            <td colSpan={2} className="text-center p-3">Hamlock&#40;1&#41;, Flatline&#40;1&#41;, R-301&#40;1&#41;, Mastiff&#40;1&#41;, Deez&#40;1&#41;, Halouani&#40;1&#41;,Hamlock&#40;1&#41;, Flatline&#40;1&#41;, R-301&#40;1&#41;, Mastiff&#40;1&#41;, Deez&#40;1&#41;, Halouani&#40;1&#41;,</td>
             <td className="text-center p-3">num 2 rue deez nuts hammem sousse sousse tunisia</td>
             <td className="text-center p-3">95237383</td>
             <td className="text-center p-3">oussema@deezhalouani.nuts</td>
-            <td className='grid place-items-center h-full'><span></span></td>
+            <td className='text-center'><span className='font-extrabold text-xl text-red-500'>&#x2716;</span></td>
         </tr>
     )
 }
