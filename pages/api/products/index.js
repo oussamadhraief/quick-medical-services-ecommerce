e38c7bf25d7336cb9e1a7843efad3fd8ccf4b003
Halouani <Halouani142@gmail.com>
2022-02-07 15:18:13 +0100

    commit

--- pages/api/products/index.js
+++ pages/api/products/index.js
@@ -1,31 +1,31 @@
 import dbConnect from "../../../utils/dbConnect";
-import Product from '../../../models/Product'
+import Product from "../../../models/Product";
 
-dbConnect()
+dbConnect();
 
-export default async (req,res) => {
-  const { method } = req
-  switch(method){
-        case 'GET':
+export default async (req, res) => {
+    const { method } = req;
+    switch (method) {
+        case "GET":
             try {
-                const products = await Product.find({})
-                
-                res.status(200).json({ success: true, data: products })
+                const products = await Product.find({});
+
+                res.status(200).json({ success: true, data: products });
             } catch (error) {
-                res.status(400).json({ success: false })
+                res.status(400).json({ success: false });
             }
-            break
-        case 'POST':
+            break;
+        case "POST":
             try {
-                const products = await Product.create(req.body)
+                const products = await Product.create(req.body);
 
-                res.status(200).json({ success: true, data: req.body})
+                res.status(200).json({ success: true, data: req.body });
             } catch (error) {
-                res.status(400).json({ success: false })
-            }  
-            break
+                res.status(400).json({ success: false });
+            }
+            break;
         default:
-            res.status(400).json({ success: false })
-            break
-  }
-}
+            res.status(400).json({ success: false });
+            break;
+    }
+};
