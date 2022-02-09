82e1e875fc5ec8152a87bfe4891fdd4ea32d42de
Oussema <Oussema.dhraief.17@gmail.com>
2022-02-09 21:50:01 +0000

    commit

--- pages/Admin.js
+++ pages/Admin.js
@@ -66,7 +66,7 @@ export default function Admin(admindata){
                 <PageView selected={selection} />
             </ProductsContext.Provider>}
         </div>
-    )
+    )//login design
 }
 
 
