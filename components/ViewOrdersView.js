ffda21ab62f8e084e0135bc8ee1ba1855c47e73d
Oussema <Oussema.dhraief.17@gmail.com>
2022-02-11 23:23:57 +0100

    commit

--- components/ViewOrdersView.js
+++ components/ViewOrdersView.js
@@ -2,7 +2,7 @@ import OrdersTable from './OrdersTable'
 
 export default function ViewOrdersView(){
     return (
-        <div className="h-full w-full border-2 border-zinc-300 rounded-md">
+        <div className="h-full w-full border-2 border-zinc-300 rounded-md overflow-y-scroll overflow-x-auto">
             <OrdersTable />
         </div>
     )
