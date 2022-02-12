b473b483d763a376821aa0f81a831104a12c6353
Oussema <Oussema.dhraief.17@gmail.com>
2022-02-12 20:05:13 +0100

    commit

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from "next/head"
 import Link from "next/link"
 import Header from "../components/Header"
 
-export default function Home(props) {
+export default function Home() {
   return (
     <div>
       <Head>
