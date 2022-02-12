a144b72b86e34d325bdc4993e1e62c635d1d49a1
Oussema <Oussema.dhraief.17@gmail.com>
2022-02-12 22:27:15 +0100

    commit

--- pages/admin.js
+++ pages/admin.js
@@ -130,8 +130,8 @@ export default function Admin(data){
     )
 }
 
-export async function getServerSideProps() {
-    const res = await fetch('https://api.imgflip.com/get_memes')
-    const data = await res.json()
-    return { props: data }
-}
+// export async function getServerSideProps() {
+//     const res = await fetch('https://api.imgflip.com/get_memes')
+//     const data = await res.json()
+//     return { props: data }
+// }
