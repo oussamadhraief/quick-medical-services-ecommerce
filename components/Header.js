900ad518b6a24dc6f7e43abac535c1bb7074a3d1
Oussema <Oussema.dhraief.17@gmail.com>
2022-02-12 13:13:48 +0100

    commit

--- components/Header.js
+++ components/Header.js
@@ -1,15 +1,11 @@
 import Navbar from "./Navbar"
 import NavigationSection from "./NavigationSection"
-import Link from "next/link"
 
 export default function Header(){
     return (
         <header>
             <Navbar />
             <NavigationSection />
-            <Link href="/Admin">
-            <a className="text-5xl">Admin</a>
-            </Link>
         </header>
     )
 }
--- pages/Admin.js
+++ pages/Admin.js
@@ -28,7 +28,7 @@ export default function Admin(admindata){
 
     const getProducts = async () => {
         try{
-            const res = await fetch('api/products',{
+            const res = await fetch('http://localhost:3000/api/products',{
                 method: 'GET',
                 headers: {
                     "Accept": "application/json",
@@ -103,7 +103,7 @@ export default function Admin(admindata){
 
 export async function getServerSideProps() {
   
-        const res = await fetch('api/admindata')
+        const res = await fetch('http://localhost:3000/api/admindata')
         const { data } = await res.json() 
 
         return {props: data[0]}
