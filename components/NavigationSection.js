55e1e8eaaa5b56ec5bc7a5a00cc7977c4082f34f
Oussema <Oussema.dhraief.17@gmail.com>
2022-02-17 01:27:57 +0100

    commit

--- components/NavigationSection.js
+++ components/NavigationSection.js
@@ -5,7 +5,6 @@ export default function NavigationSection(){
     return(
         <nav  className="w-full relative grid md:flex flex-nowrap justify-center z-10 items-center py-3">
             <SearchAndNavigation />
-            
         </nav>
     )
 }
--- components/SearchAndNavigation.js
+++ components/SearchAndNavigation.js
@@ -10,7 +10,7 @@ export default function SearchAndNavigation(){
         <div className="flex relative flex-nowrap px-3 mx-5 py-3 bg-white border-[1px] h-20 rounded-lg w-11/12 md:w-4/6 xl:w-3/6">
             <CategoriesMenu />
             <input type="text" name="search" value="" className="rounded-r-lg outline-none text-lg px-4 w-full" placeholder="Chercher un produit..."/>
-            <div className="w-24 absolute -top-5 right-0 z-0 h-36 bg-transparent">
+            <div className="w-28 absolute -top-5 right-0 z-0 h-36 bg-transparent">
                 <Image src={syringe} alt='design' layout="fill" />
             </div>
         </div>
