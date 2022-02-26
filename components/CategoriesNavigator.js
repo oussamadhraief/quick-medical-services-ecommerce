3fba134912df400472f703538d5b5400661cb368
Oussema Dhraief <oussema.dhraief.17@gmail.com>
2022-02-26 13:29:36 +0100

    commit

--- components/CategoriesNavigator.js
+++ components/CategoriesNavigator.js
@@ -4,7 +4,7 @@ export default function CategoriesNavigator({categoriesAndSubcategories}){
     return(
         <>
         {categoriesAndSubcategories.map(item => 
-                        <ul id={item.category} className='ml-3 my-5'><span onClick={e => {
+                        <ul id={item.category} key={item.category} className='ml-3 my-5'><span onClick={e => {
                             const element = document.querySelectorAll(`#${item.category} .expandable`)
                             if(element[0].offsetHeight < 24 ) {
                                 element.forEach(elem => {
@@ -20,7 +20,7 @@ export default function CategoriesNavigator({categoriesAndSubcategories}){
                                 })
                             }
                             }} className="hover:cursor-pointer relative ml-4 font-medium text-sm pl-2 pr-5 py-1">&#62; shsrthrtsdhrthdrthdhdrty</span> 
-                            {item.subcategories.map(element => <li className="bg-zinc-200 text-sm relative ml-12 transition-[height] duration-300 expandable h-0 w-7/12 shadow-lg invisible mt-0 min-w-fit rounded-lg font-medium flex justify-start items-center px-1">
+                            {item.subcategories.map(element => <li key={element} className="bg-zinc-200 text-sm relative ml-12 transition-[height] duration-300 expandable h-0 w-7/12 shadow-lg invisible mt-0 min-w-fit rounded-lg font-medium flex justify-start items-center px-1">
                                 <Link href='/'>
                                     <a>{element}</a>
                                 </Link>
