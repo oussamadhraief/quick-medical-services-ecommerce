2689857b18a389d75336652867cc290a679d1c00
Oussema Dhraief <oussema.dhraief.17@gmail.com>
2022-02-26 16:05:53 +0100

    commit

--- components/Header.js
+++ components/Header.js
@@ -32,8 +32,11 @@ export default function Header(){
         <header className="bg-white mb-32">
             <Navbar />
             <NavigationSection />
-            <div id="cart" className={scrolled ? `text-medium font-medium bg-white rounded-full shadow-stylish right-5 text-third fixed hover:cursor-pointer h-fit w-fit pt-2 pb-0 px-1.5 z-[9999]` : `text-medium font-medium text-third fixed h-fit w-fit hover:cursor-pointer z-[9999]`}><Image src={cart} alt='cart icon' width={37} height={32} layout='fixed' objectFit="contain" objectPosition='center' /></div>
-            <div id="positioning" className="fixed bottom-0.5 right-1 w-14 h-14">
+            <div id="cart" className={scrolled ? `text-medium font-medium bg-white rounded-full shadow-stylish right-5 text-third fixed hover:cursor-pointer h-fit w-fit pt-2 pb-0 px-1.5 z-[9999]` : `text-medium font-medium text-third fixed h-fit w-fit hover:cursor-pointer z-[9999]`}>
+                <Image src={cart} alt='cart icon' width={37} height={32} layout='fixed' objectFit="contain" objectPosition='center' />
+                <p className={scrolled ? "absolute bg-third border shadow-stylish rounded-full w-fit h-fit top-0 -right-2.5 text-white font-medium text-sm px-1.5 text-center" : 'absolute bg-third border shadow-stylish rounded-full w-fit h-fit -top-1 -right-3 text-white font-medium text-xs px-1.5 text-center'}>2</p>
+            </div>
+            <div id="positioning" className="fixed bottom-0.5 right-1.5 w-14 h-14">
                 
             </div>
         </header>
