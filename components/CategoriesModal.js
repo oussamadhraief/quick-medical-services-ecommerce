b30358cf777d94dfc6d89455b7c24e1b2b26859b
Oussema Dhraief <oussema.dhraief.17@gmail.com>
2022-03-04 20:22:18 +0100

    commit

--- components/CategoriesModal.js
+++ components/CategoriesModal.js
@@ -32,11 +32,11 @@ function CategoriesModal({show, onClose}) {
             <button className="w-fit h-fit absolute right-5 hover:scale-125 top-1 font-medium font-mono text-xl" onClick={e => handleCloseClick(e)}>X</button>
             <div className="w-[13%] min-h-[95vh] h-fit overflow-auto border-r flex flex-col border-third gap-8 px-3 pt-5">
                 {categoriesAndSubcategories.map((item,index) => 
-                    <button className={index != selected ? "w-full min-w-full h-fit px-2 py-1 text-third font-medium flex justify-between items-center hover:bg-third hover:text-white hover:rounded-lg" : "w-full min-w-full h-fit px-2 py-1  font-medium flex justify-between items-center bg-third text-white rounded-lg"} onClick={e => setSelected(index)}><p className="hover:underline" onClick={e => location.href=`/categories/${item.category}`}>{item.category}</p><p> &#62;</p></button>
+                    <button key={index} className={index != selected ? "w-full min-w-full h-fit px-2 py-1 text-third font-medium flex justify-between items-center hover:bg-third hover:text-white hover:rounded-lg" : "w-full min-w-full h-fit px-2 py-1  font-medium flex justify-between items-center bg-third text-white rounded-lg"} onClick={e => setSelected(index)}><p className="hover:underline" onClick={e => location.href=`/categories/${item.category}`}>{item.category}</p><p> &#62;</p></button>
                 )}
             </div>
             <div className="w-9/12 h-[95vh] gap-8 flex-col flex-wrap flex pt-5 px-10">
-                {subcats.map(item =>  <p className="w-fit h-fit font-medium flex flex-nowrap items-center">&#62;&nbsp;<p className="hover:underline hover:cursor-pointer" onClick={e => location.href=`/categories/${categoriesAndSubcategories[selected].category}/${item}`}>
+                {subcats.map(item =>  <p key={item} className="w-fit h-fit font-medium flex flex-nowrap items-center">&#62;&nbsp;<p className="hover:underline hover:cursor-pointer" onClick={e => location.href=`/categories/${categoriesAndSubcategories[selected].category}/${item}`}>
                   {item}</p></p>)}
             </div>
         </div>
