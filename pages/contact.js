a2ba28f0414e05d0fb6316a7de37c5b8a9ad6b60
Halouani <Halouani142@gmail.com>
2022-03-05 02:04:38 +0100

    contact form much cooler

--- pages/contact.js
+++ pages/contact.js
@@ -32,33 +32,49 @@ export default function Contact() {
                 <Header landingPage={false} />
             </CategoriesContext.Provider> */}
             <div className="flex w-3/4  mx-auto shadow-md mt-20 rounded-2xl">
-                <div className="w-1/2 h-full bg-gradient-to-br from-na3ne3i to-light mx-auto ">
+                <div className="w-1/2 backgroundAnimated mx-auto ">
                     
                     <div className="w-9/12 mx-auto mt-12 mb-8">
-                        <h1 className="text-white text-3xl">Contact us</h1> 
+                        <h1 className="text-white text-4xl font-bold">Contact us</h1> 
                         <hr className="w-4/12"/>
-                        <p className="text-white text-sm"> dont hesitate to contact us , we're open for suggestion</p> 
+                        <p className="text-white text-md mt-2 font-medium"> dont hesitate to contact us , we're open for suggestion</p> 
                     </div>
                     
                     <div className="w-9/12 mx-auto space-y-4 ">
                         <Link href='https://www.facebook.com/QMSJRIBI'>
-                            <a target='_blank' className="flex text-white items-center flex-nowrap whitespace-nowrap"><Image src={facebook} alt='facebook' width={16} height={16} layout='fixed' />&nbsp;&nbsp;&nbsp;Facebook: QUICK Medical services</a>
+                            <a target='_blank' className="flex text-white items-center flex-nowrap whitespace-nowrap font-medium"><Image src={facebook} alt='facebook' width={18} height={18} layout='fixed' />&nbsp;&nbsp;&nbsp;Facebook: QUICK Medical services</a>
                         </Link>
 
-                        <p className="whitespace-nowrap text-white ">&#x2709;&nbsp;&nbsp; Email: qms.jribi@gmail.com</p>
+                        <p className="whitespace-nowrap text-white font-medium ">&#x2709;&nbsp;&nbsp; Email: qms.jribi@gmail.com</p>
 
-                        <p className="whitespace-nowrap text-white">&#x260E;&nbsp;&nbsp; Tel: +216 44 219 218</p>
-                            
-                        <div className="flex items-center flex-nowrap text-white">
-                            <Image src={location} alt='location' width={20} height={20} layout='fixed' />
-                            <p className="whitespace-nowrap">&nbsp;&nbsp;Sfax, Tunisie</p>
-                        </div>
-
+                        <p className="whitespace-nowrap text-white font-medium">&#x260E;&nbsp;&nbsp; Tel: +216 44 219 218</p>
+                         
                     </div>
                 </div>
-                <div className="w-1/2 h-full bg-white"></div>
-                
-            </div>
+
+                    <form className=" w-1/2 h-full bg-white mx-auto my-12 space-y-5">
+                        <div className="relative mx-auto w-11/12 h-fit">
+                            <input className="form-input peer" type="text" name="name" id="formName"  />
+                            <label className="absolute text-zinc-400  hover:cursor-text top-3 left-2 peer-focus:text-third peer-focus:left-0 peer-focus:-top-3 w-fit h-fit transition-all" for="formName">Nom et Prénom</label>
+                        </div>
+                        <div className="relative mx-auto w-11/12 h-fit">
+                            <input className="form-input peer" type="email" name="email" id="formEmail"  />
+                            <label className="absolute text-zinc-400  hover:cursor-text top-3 left-2 peer-focus:text-third peer-focus:left-0 peer-focus:-top-3 w-fit h-fit transition-all" for="formEmail">Email</label>
+                        </div>
+                        <div className="relative mx-auto w-11/12 h-fit">
+                            <input className="form-input peer" type="tel" name="phoneNumber" id="formPhoneNumber"  />
+                            <label className="absolute text-zinc-400  hover:cursor-text top-3 left-2 peer-focus:text-third peer-focus:left-0 peer-focus:-top-3 w-fit h-fit transition-all" for="formPhoneNumber">Num. de téléphone</label>
+                        </div>
+                        <div className="relative mx-auto w-11/12 h-fit">
+                            <input className="form-input peer" type="text" name="subject" id="formSubject"  />
+                            <label className="absolute text-zinc-400  hover:cursor-text top-3 left-2 peer-focus:text-third peer-focus:left-0 peer-focus:-top-3 w-fit h-fit transition-all" for="formSubject">Sujet</label>
+                        </div>
+                        <div className="relative mx-auto w-11/12 h-fit">
+                            <textarea className="h-32 form-input peer" col={50} row={4} name="textArea" id="formTextArea"></textarea>
+                            <label className="absolute text-zinc-400  hover:cursor-text top-3 left-2 peer-focus:text-third peer-focus:left-0 peer-focus:-top-3 w-fit h-fit transition-all" for="formTextArea">Message</label>
+                        </div>
+                    </form>
+                </div>
             <Footer/>
         </div>
             
