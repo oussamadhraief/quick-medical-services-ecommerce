79a2c9b4cab220fce4ddf73c2b4d8cb351f60092
Oussema Dhraief <oussema.dhraief.17@gmail.com>
2022-04-17 04:41:03 +0100

    c

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,8 +9,8 @@ module.exports = {
         '3xl': '0px 3px 16px 5px rgba(125,125,125,0.6)',
         stylish: '0px 5px 20px 1px rgba(0,0,0,0.4)',
         form:
-          'rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px;',
-        btn: 'rgba(0, 0, 0, 0.2) 0px 60px 40px -7px;',
+          'rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px',
+        btn: 'rgba(0, 0, 0, 0.2) 0px 60px 40px -7px',
         float: '0 8px 55px 0 rgb(0 0 0 / 16%)'
       },
       screens: {
