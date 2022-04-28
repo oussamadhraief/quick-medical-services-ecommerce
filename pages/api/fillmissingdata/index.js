01487ef5f85000b74d8d13df9f3057f77591daaf
HalouaniMohamed <Halouani142@gmail.com>
2022-04-28 22:50:59 +0100

    commit

--- pages/api/fillmissingdata/index.js
+++ pages/api/fillmissingdata/index.js
@@ -1,20 +1,27 @@
 import dbConnect from "../../../utils/dbConnect";
 import Docteur from "../../../Models/Docteur";
+import { getSession } from "next-auth/react"
 
 dbConnect();
 export default  async (req, res) => {
+  const session = await getSession({ req })
   if (req.method !== 'POST') return res.status(400).json({success : false , message :'Only POST request accepted'})
-  try {
-    let dataToUpdate = {}
-    if(req.body.phone != '') dataToUpdate.phone = req.body.phone
-    if(req.body.address != '') dataToUpdate.address = req.body.address
-    const userData = await Docteur.findOneAndUpdate({email : req.body.email},
-        dataToUpdate,
-        { new: true, runValidators: true })
-  
-    res.status(200).json({ success: true, data: userData });
-  } catch (error) {
-    res.status(400).json({ success: false });
+  if(session){
+    try {
+      let dataToUpdate = {}
+      if(req.body.phone != '') dataToUpdate.phone = req.body.phone
+      if(req.body.address != '') dataToUpdate.address = req.body.address
+      const userData = await Docteur.findOneAndUpdate({email : req.body.email},
+          dataToUpdate,
+          { new: true, runValidators: true })
+    
+      res.status(200).json({ success: true, data: userData });
+    } catch (error) {
+      res.status(400).json({ success: false });
+    }
+  }
+  else{
+    res.status(401).json({message: 'Must be authenticated'})
   }
 }
 
--- pages/api/orders/index.js
+++ pages/api/orders/index.js
@@ -1,24 +1,47 @@
 import dbConnect from "../../../utils/dbConnect"
 import Commande from "../../../Models/Commande"
+import { getSession } from "next-auth/react"
 
 dbConnect();
 
 export default async (req, res) => {
+    const session = await getSession({ req })
     switch (req.method) {
         case "GET":
             try {
-                const commande = await Commande.find({});
-                res.status(200).json({ success: true, data: commande });
-            } catch {
+                if(session){
+                    if(session.user.isAdmin){
+                        const commande = await Commande.find({});
+                        res.status(200).json({ success: true, data: commande });
+                    }
+                    else{
+                        console.log('Must be authorized')
+                        res.status(401).json({success: false , message: 'Must be authorized'})
+                    }
+                }
+                else{
+                    console.log('Must be authenticated')
+                    res.status(401).json({success: false , message: 'Must be authenticated'})
+                }
+                
+            } catch(error) {
+                console.error(error)
                 res.status(400).json({ success: false });
             }
             break;
         case "POST":
             try {
-                const commande = await Commande.create(req.body);
-                res.status(201).json({ success: true, data: commande });
+                if(session){
+                    const commande = await Commande.create(req.body);
+                    res.status(201).json({ success: true, data: commande }); 
+                }
+                else{
+                    console.log('Must be authenticated')
+                    res.status(401).json({success: false , message: 'Must be authenticated'})
+                }
                 
             } catch (error) {
+                console.error(error)
                 res.status(400).json({ success: false });
             }
             break;
