4081dc2e792a9dd3e94b75fb4588b4e1441d14ea
Oussema Dhraief <oussema.dhraief.17@gmail.com>
2022-06-13 13:19:56 +0100

    commit

--- pages/api/user/makeorder.js
+++ pages/api/user/makeorder.js
@@ -9,51 +9,59 @@ export default async function handler (req, res) {
   const session = await getSession({ req })
 
   if (req.method !== 'POST') return
-  if (session) {
+  try {
+    if (session) {
     
-    const User = await Brimstone.findOne({ email: session.user.email })
-    if(!User){
-      res.status(404).json({success: false, message: 'User not found'})
-    }
-
-    let orderData = []
-
-    for (const item of req.body.cart) {
-      let product = await Instrument.findOne({ reference: item.reference })
-      if(!product){
-        res.status(404).json({success: false , message: 'Product not found'})
+      const User = await Brimstone.findOne({ email: session.user.email })
+      if(!User){
+        res.status(404).json({success: false, message: 'User not found'})
+        return
       }
-      
-      orderData.push({
-        product: product,
-        size: item.size,
-        quantity: item.quantity
+  
+      let orderData = []
+  
+      for (const item of req.body.cart) {
+        let product = await Instrument.findOne({ reference: item.reference })
+        if(!product){
+          res.status(404).json({success: false , message: 'Product not found'})
+          return
+        }
+        
+        orderData.push({
+          product: product,
+          size: item.size,
+          quantity: item.quantity
+        })
+      }
+  
+      const commande = await Amazon.create({
+        user: User,
+        email: req.body.email,
+        cart: orderData,
+        name: req.body.name,
+        address : req.body.address,
+        city : req.body.city,
+        country : req.body.country,
+        zipCode : req.body.zipCode,
+        phoneNumber : req.body.phone,
+        clinicName: req.body.clinicName,
+        taxRegistrationNumber: req.body.taxRegistrationNumber,
+        note: req.body.note
       })
+  
+      User.ordersHistory.push(commande)
+      User.cart = []
+      User.save()
+  
+      res.status(201).json({ success: true, data: commande, user: User })
+  
+      
+    } else {
+      res.status(401).json({ sucess: false, message: 'must be authenticated' })
     }
-
-    const commande = await Amazon.create({
-      user: User,
-      email: req.body.email,
-      cart: orderData,
-      name: req.body.name,
-      address : req.body.address,
-      city : req.body.city,
-      country : req.body.country,
-      zipCode : req.body.zipCode,
-      phoneNumber : req.body.phone,
-      clinicName: req.body.clinicName,
-      taxRegistrationNumber: req.body.taxRegistrationNumber,
-      note: req.body.note
-    })
-
-    User.ordersHistory.push(commande)
-    User.cart = []
-    User.save()
-
-    res.status(201).json({ success: true, data: commande, user: User })
-
+  } catch (error) {
+    res.status(400).json({ sucess: false })
     
-  } else {
-    res.status(401).json({ sucess: false, message: 'must be authenticated' })
   }
+  
 }
