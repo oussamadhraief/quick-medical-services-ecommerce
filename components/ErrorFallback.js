18a64abfdcd32197ee48008908895cdf01478fb0
Oussema Dhraief <oussema.dhraief.17@gmail.com>
2022-06-18 16:10:37 +0100

    commit

---
+++ components/ErrorFallback.js
@@ -0,0 +1,9 @@
+
+
+export default function ErrorFallback(){
+    return (
+        <section>
+            error
+        </section>
+    )
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import '../styles/globals.css'
 import {SessionProvider} from "next-auth/react"
-import { useSession } from "next-auth/react"
+import ErrorBoundary from '../components/ErrorBoundary'
+import ErrorFallback from '../components/ErrorFallback'
 
 
 
@@ -11,29 +12,9 @@ export default function MyApp({
 
   return (
     <SessionProvider session={session}  >
-      {Component.auth ? (
-        <Auth>
-          <Component {...pageProps} />
-        </Auth>
-      ) : (
+      <ErrorBoundary FallbackComponent={ErrorFallback}>
         <Component {...pageProps} />
-      )}
+        </ErrorBoundary>
     </SessionProvider>
   )
-}
-
-function Auth({ children }) {
-  const { status } = useSession({ required: true})
-
-  if (status === 'loading') {
-    return (
-      <div className='bg-white h-screen w-screen overflow-hidden flex items-center absolute z-[9999] left-0 top-0'>
-        <div id="contact-loading" className="w-fit h-fit bg-white/70 z-[9999] mx-auto ">
-          <div className="reverse-spinner "></div>
-        </div>
-      </div>
-     )
-    }
-
-  return children
 }
